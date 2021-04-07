8bc31d3ec132914e060abccc3d7166860e9fccc8
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-04-07 11:54:05 +0000

    Modifications de la page de détails d'un personnage

--- Source/App/Vue_perso/NouveauPerso.xaml.cs
+++ Source/App/Vue_perso/NouveauPerso.xaml.cs
@@ -21,5 +21,10 @@ namespace Vue_perso
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
