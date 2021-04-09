84948042880bcdfa671287c8f377068a69a13934
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-04-09 10:03:30 +0000

    Ajout d'un sketch pour les détails sur un personnage et modification de la fenêtre sur les détails d'un personnage (ajout d'un menu, changement du background)

--- Source/App/Vue_perso/MainWindow.xaml.cs
+++ Source/App/Vue_perso/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Vue_perso
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
