7fba69ef3daa6d1639e5ec79feec558be34c95e7
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-04-29 07:50:38 +0000

    Modifications sur le diagramme de classe et exportation en .png pour y accéder sans StarUML
    Création d'une bibliothèque de classe et début de création des classes JeuVideo, Nommable, Personnage, Relation, ThemeMusical et Titre (avec notes en commentaire multiligne au début de certains fichiers)

---
+++ trunk/Source/App/Classes/Personnage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Personnage : Nommable
+    {
+        // Champs
+        public List<string> Citations { get; set; };
+        public string Image { get; set; }
+        public List<JeuVideo> JeuxVideo { get; set; }
+        public ThemeMusical Theme { get; set; }
+        public List<Relation> Relations { get; set; }
+
+        // Méthodes
+        public Personnage(string nom) : base(nom)
+        {
+            // INITIALISER LES LISTES ?
+        }
+    }
+}
