2f6b9fbe99ef991906573bfcc04ef1aa48fe213d
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-04-29 06:21:37 +0000

    Test de tag

---
+++ tags/test/Source/App/Vue_perso/Accueil.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vue_perso
+{
+    /// <summary>
+    /// Logique d'interaction pour Accueil.xaml
+    /// </summary>
+    public partial class Accueil : Window
+    {
+        public Accueil()
+        {
+            InitializeComponent();
+        }
+
+        private void Affiche_Perso(object sender, RoutedEventArgs e)
+        {
+            contentControlAccueil.Content = new MosaiquePersonnages_UC();
+        }
+
+        private void Affiche_Jeux(object sender, RoutedEventArgs e)
+        {
+            contentControlAccueil.Content = new MosaiqueJeux_UC();
+        }
+    }
+}
