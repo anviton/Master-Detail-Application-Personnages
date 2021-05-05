1c1d888790b599166ae70ff678ca48e86e952084
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-05 06:38:45 +0000

    Ecriture des protocoles d'égalité pour les classes Personnage et Relation. Modification de l'attribut nom de la classe Nommable création d'une propriété Nom. Dans Personnage changement de structure de données pour les relation utilisation d'un HashSet.

--- trunk/Source/App/Classes/Personnage.cs
+++ trunk/Source/App/Classes/Personnage.cs
@@ -4,19 +4,68 @@ using System.Text;
 
 namespace Classes
 {
-    public class Personnage : Nommable
+    public class Personnage : Nommable, IEquatable<Personnage>
     {
         // Champs
+
+        //Atributs
+        public HashSet<Relation> relations;
+        //Propriétés
         public IList<string> Citations { get; set; }
         public string Image { get; set; }
         public IList<JeuVideo> JeuxVideo { get; set; }
         public ThemeMusical Theme { get; set; }
-        public IList<Relation> Relations { get; private set; }
+
+        //Protocole d'égalité
+        /// <summary>
+        /// Permet de vérifier si relation et obj sont égaaux
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals(obj as Personnage);
+        }
+
+        /// <summary>
+        /// Permet de vérifier si les champs Type, NomPersoNonRec, PersoRec relation et obj sont égaux
+        /// </summary>
+        /// <param name="autre"></param>
+        /// <returns></returns>
+        public bool Equals(Personnage autre)
+        {
+            return (this.Nom==autre.Nom);
+        }
+
+        /// <summary>
+        /// Retourne un hashcode pour utiliser cete classe dans une table de hashage
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+
+            return Nom.GetHashCode() % 31;
+        }
 
         // Méthodes
         public Personnage(string nom) : base(nom)
         {
-            Relations = new List<Relation>();
+            relations = new HashSet<Relation>();
             // INITIALISER LES LISTES ?
         }
 
@@ -27,7 +76,7 @@ namespace Classes
         /// <param name="perso">Personnage avec lequel la relation est réalisée</param>
         public void AjouterRelation(string type, Personnage perso)
         {
-            Relations.Add(new Relation(type, perso));
+            relations.Add(new Relation(type, perso));
         }
 
         /// <summary>
@@ -37,7 +86,7 @@ namespace Classes
         /// <param name="nomPerso">Nome du Personnage avec lequel la relation est réalisée</param>
         public void AjouterRelation(string type, string nomPerso)
         {
-            Relations.Add(new Relation(type, nomPerso));
+            relations.Add(new Relation(type, nomPerso));
         }
 
 
@@ -47,21 +96,41 @@ namespace Classes
         /// <returns>Retourne toutes les Relations du personnage sous forme de chaine de caractères</returns>
         public string AfficherLesRelations()
         {
-            string lesRelations = $"Les Relations de {nom} :";
-            if (Relations.Count == 0)
+            string lesRelations = $"Les Relations de {Nom} :";
+            if (relations.Count == 0)
                 return lesRelations + "Aucune";
 
             else
-                for (int i = 0; i< Relations.Count ; i++) 
+                foreach (Relation relation in relations) 
                 {
-                    if(Relations[i].NomPersoNonRec == null)
+                    if(relation.NomPersoNonRec == null)
                     {
-                        lesRelations = lesRelations + $"\n {Relations[i].PersoRec.nom} : {Relations[i].Type}";
+                        lesRelations = lesRelations + $"\n {relation.PersoRec.Nom} : {relation.Type}";
                     }
                     else
-                        lesRelations = lesRelations + $"\n {Relations[i].NomPersoNonRec} : {Relations[i].Type}";
+                        lesRelations = lesRelations + $"\n {relation.NomPersoNonRec} : {relation.Type}";
                 }
+            
                 return lesRelations;
         }
+
+        /// <summary>
+        /// Permet de supprimer une relatiuon de la liste des relations
+        /// </summary>
+        /// <param name="relation">relation à supprimer</param>
+       public void SupprimerUneRelation(Relation relation)
+        {
+            relations.Remove(relation);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="relation"></param>
+        public void ModifierUneRelation(Relation relation)
+        {
+            
+        }
+
     }
 }
