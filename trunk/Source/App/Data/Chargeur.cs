bd86fd71678991a5386c8a7daaa4288a16412a4c
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-06 09:55:14 +0000

    Début de la création du chargeur et du stub dans le projet Data que j'ai ajouté. Modification de la classe Donnees.

---
+++ trunk/Source/App/Data/Chargeur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Classes;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public abstract class Chargeur
+    {
+        private string cheminDAcces;
+        public Chargeur(string chemin)
+        {
+            cheminDAcces = chemin;
+        }
+
+        public abstract Donnees Charger(string chemin);
+    }
+}
