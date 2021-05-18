fa63c31f4a46bf52f0cb05598ffd2e943699af8f
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-18 19:58:35 +0000

    Réorganisation des tests fonctionnels création du projet Tests_Series_et_Groupes.

--- trunk/Source/App/Test_Personnage/Program.cs
+++ trunk/Source/App/Test_Personnage/Program.cs
@@ -9,47 +9,16 @@ namespace Test_Personnage
     {
         static void Main(string[] args)
         {
-
-            /*Personnage perso1 = new Personnage("Mario");
-            Personnage perso2 = new Personnage("Bowser");
-            Serie Mario = new Serie("mario");
-            Mario.AjouterUnPersonnage(perso1);
-            Mario.AjouterUnPersonnage(perso1);
-            Mario.AjouterUnPersonnage(perso2);
-            Mario.Personnages[Mario.Personnages.IndexOf(perso1)].AjouterRelation("Ennemi", perso2);
-            AfficherUneSerie(Mario);
-            Console.WriteLine($"Mon nom est {perso1.Nom}");
-            Console.WriteLine(AfficherLesRelations(perso1));
-           //Console.WriteLine(perso1.ToString());
-            
-            perso1.AjouterRelation("frère", "Luigi");
-            Console.WriteLine(AfficherLesRelations(perso1));
-            Console.WriteLine(AfficherLesRelations(perso2));
-            Console.WriteLine("Test contenu dans la liste :");
-            // !!! MAUVAIS TEST : UTILISER UNE FONCTION ADAPTÉE
-            if (perso1.Relations.Contains(new Relation("frère", "Luigi")))
-                Console.WriteLine("OK");
-            else
-                Console.WriteLine("Pas OK");
-            perso1.SupprimerUneRelation(new Relation("enfant", "Mario"));
-            Console.WriteLine("Test supprimer une relation : ");
-            if (perso1.Relations.Contains(new Relation("enfant", "Mario")))
-                Console.WriteLine("Pas OK");
-            else
-                Console.WriteLine("OK");
-
-            //Console.WriteLine($"Mon nom est {perso1.nom} relation : ");*/
             Chargeur chargeur1 = new Stub("");
             Manager manager = chargeur1.Charger();
-            //Test de l'affichage de toutes les série
-            AfficherLesSeries(manager.Series);
-
-
-            //Test de l'affichage des membres d'une série
+            //Test de l'affichage des relations d'un personnage (ayant des relations) (+ ajout et suppression Relation)
             manager.Series.TryGetValue(new Serie("mario"), out Serie serie);
-            AfficherUneSerie(serie);
-            //Test de l'affichage des relations d'un personnage (ayant des relations)
             serie.Personnages.TryGetValue(new Personnage("Mario", "mario"), out Personnage perso1);
+            AfficherLesRelations(perso1);
+            manager.RechercherUnPersonnage("Link", "zelda", out Personnage perso1_2);
+            perso1.AjouterRelation("Copain", perso1_2);
+            manager.RechercherUnPersonnage("Bowser", "mario", out Personnage perso1_3);
+            perso1.SupprimerUneRelation(new Relation("Ennemi", perso1_3));
             AfficherLesRelations(perso1);
             //Test de l'afichage des relations d'un personnage (n'ayant pas de relations)
             serie.Personnages.TryGetValue(new Personnage("Bowser", "mario"), out Personnage perso2);
@@ -60,42 +29,35 @@ namespace Test_Personnage
             AfficherLalisteDesJeuxDunPerso(perso1);
             perso1.AjouterUnJeu("Super Mario Bros.", 1985);
             AfficherLalisteDesJeuxDunPerso(perso1);
+            //Test de l'affichage de la description d'un oerso
+            Console.WriteLine($"\nLa descrption de {perso1.Nom} :");
+            Console.WriteLine($"{perso1.Description}");
             //Test de l'affichage du thème musical d'un perso
             AfficherLeThemeMusicalDUnPersonnage(perso2);
+            // A compléter
+
+            //Test de l'affichage des citations
+            manager.RechercherUnPersonnage("Madeline", "celeste", out Personnage perso4);
+            AfficherLalisteDesCitationsDunPerso(perso4);
+            perso4.SupprimerCitation("This is it, Madeline. Just breathe.");
+            perso4.AjouterCitation("Tests");
+            AfficherLalisteDesCitationsDunPerso(perso4);
             //Test de l'affichage de tous les personnages de l'application
             AfficherTousLesPersonnages(manager);
-            //Test de l'affichage de tous les groupes de personnages de l'application
-            AfficherLaListeDesGroupes(manager.Groupes);
-            //Test de l'affichage d'un groupe
-            AfficherUnGroupe("Triforce", manager);
-            //Test rechercher un personnage
+            //Test Enregistrer un personnage
             Serie test;
             manager.RechercherUneSerie("zelda", out test);
             manager.EnregistrerPersonnage("Bowser", test, out Personnage perso3);
             Console.WriteLine();
+            //Test rechercher un personnage
             Console.WriteLine("Je recherche le personnage \"bowser\" :");
             TestRechercherUnPerso("Bowser", "mario", manager);
             Console.WriteLine();
             Console.WriteLine("Je recherche le personnage \"bowse\" :");
             TestRechercherUnPerso("Bowse", "mario", manager);
-            //Test rechercher une série
-            Console.WriteLine();
-            Console.WriteLine("Je recherche la série \"mario\" :");
-            TestRechercherUneSerie("mario", manager);
-            Console.WriteLine();
-            Console.WriteLine("Je recherche la série \"mari\" :");
-            TestRechercherUneSerie("mari", manager);
-
         }
 
-        private static void AfficherUneSerie (Serie serie)
-        {
-            Console.WriteLine($"\nLa Série \"{serie.Nom}\" est composée de :");
-            foreach (Personnage personnage in serie.Personnages)
-            {
-                Console.WriteLine(personnage);
-            }
-        }
+        
 
         private static void AfficherLesRelations(Personnage personnage)
         {
@@ -128,48 +90,22 @@ namespace Test_Personnage
 
         private static void AfficherLeThemeMusicalDUnPersonnage(Personnage personnage)
         {
-            Console.WriteLine($"Le théme de {personnage.Nom} :");
+            Console.WriteLine($"\nLe théme de {personnage.Nom} :");
             Console.WriteLine(personnage.Theme);
         }
 
-        private static void AfficherLesSeries(HashSet<Serie> Series)
-        {
-            Console.WriteLine("\nLa liste des séries :");
-            foreach (Serie serie in Series)
-            {
-                Console.WriteLine(serie);
-            }
-        }
+        
 
         private static void AfficherTousLesPersonnages(Manager manager)
         {
-            var tousLesPerso = manager.Series.SelectMany(serie => serie.Personnages)
-                                                .Distinct();
             Console.WriteLine("\nTous les personnages de l'application :");
-            foreach (Personnage personnage in tousLesPerso)
+            foreach (Personnage personnage in manager.Personnages)
             {
                 Console.WriteLine(personnage);
             }
 
         }
-        private static void AfficherLaListeDesGroupes(IDictionary<string, HashSet<Personnage>> groupes)
-        {
-            Console.WriteLine("\nListe des groupes : ");
-            foreach ( string nomGroupe in groupes.Keys)
-            {
-                Console.WriteLine(nomGroupe);
-            }
-        }
-
-        private static void AfficherUnGroupe(string nom, Manager manager)
-        {
-            manager.Groupes.TryGetValue(nom, out HashSet<Personnage> personnages);
-            Console.WriteLine($"\nLe groupe \"{nom}\" est composé de :");
-            foreach (Personnage personnage in personnages)
-            {
-                Console.WriteLine(personnage);
-            }
-        }
+        
 
         public static void TestRechercherUnPerso(string nom, string nomSerie, Manager manager)
         {
@@ -183,19 +119,15 @@ namespace Test_Personnage
             }
         }
 
-        public static void TestRechercherUneSerie(string nom, Manager manager)
+        private static void AfficherLalisteDesCitationsDunPerso(Personnage personnage)
         {
-            if (manager.RechercherUneSerie(nom, out Serie serie))
+            Console.WriteLine($"\nLes citations de {personnage.Nom} sont : ");
+            foreach (string citation in personnage.Citations)
             {
-                Console.WriteLine(serie);
+                Console.WriteLine(citation);
             }
-            else
-            {
-                Console.WriteLine("Serie inxesistante");
-            }
+
         }
 
-
-
-    }
+        }
 }
