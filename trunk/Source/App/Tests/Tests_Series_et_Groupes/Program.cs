6fda34f1f0b058a3b18d16c54ec5eb7c1d8f8cda
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-18 20:06:15 +0000

    Correction révision 84 et 85 publication du contenu du fichier Tests_Series_et_Groupes

---
+++ trunk/Source/App/Tests/Tests_Series_et_Groupes/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Modele;
+using System.Collections.Generic;
+using Data;
+using System.Linq;
+namespace Tests_Series_et_Groupes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chargeur chargeur1 = new Stub("");
+            Manager manager = chargeur1.Charger();
+            Console.WriteLine("Tests sur les SERIES ------");
+            //Test de l'affichage de toutes les série
+            AfficherLesSeries(manager.Series);
+            //Test de l'affichage des membres d'une série
+            manager.Series.TryGetValue(new Serie("mario"), out Serie serie);
+            AfficherUneSerie(serie);
+            //Test rechercher une série
+            Console.WriteLine();
+            Console.WriteLine("Je recherche la série \"mario\" :");
+            TestRechercherUneSerie("mario", manager);
+            Console.WriteLine();
+            Console.WriteLine("Je recherche la série \"mari\" :");
+            TestRechercherUneSerie("mari", manager);
+            Console.WriteLine("\nTests sur les GROUPES ------");
+            //Test de l'affichage de tous les groupes de personnages de l'application
+            AfficherLaListeDesGroupes(manager.Groupes);
+            //Test de l'affichage d'un groupe
+            AfficherUnGroupe("Triforce", manager);
+            //Test Ajouter un groupe + Ajouter Personnage a un groupe + supprimer un groupe
+            Console.WriteLine("\nTests ajouter un groupe et ajouter perso à un groupe");
+            manager.AjouterGroupe("toto");
+            Personnage perso;
+            manager.RechercherUnPersonnage("Bowser", "mario", out perso);
+            manager.AjouterPersoAGroupe("toto", perso);
+            AfficherLaListeDesGroupes(manager.Groupes);
+            AfficherUnGroupe("toto", manager);
+            manager.RetirerPersoDeGroupe("toto", perso);
+            AfficherUnGroupe("toto", manager);
+            manager.SupprimerGroupe("toto");
+            AfficherLaListeDesGroupes(manager.Groupes);
+        }
+
+        private static void AfficherUneSerie(Serie serie)
+        {
+            Console.WriteLine($"\nLa Série \"{serie.Nom}\" est composée de :");
+            foreach (Personnage personnage in serie.Personnages)
+            {
+                Console.WriteLine(personnage);
+            }
+        }
+
+        private static void AfficherLesSeries(HashSet<Serie> Series)
+        {
+            Console.WriteLine("\nLa liste des séries :");
+            foreach (Serie serie in Series)
+            {
+                Console.WriteLine(serie);
+            }
+        }
+
+        public static void TestRechercherUneSerie(string nom, Manager manager)
+        {
+            if (manager.RechercherUneSerie(nom, out Serie serie))
+            {
+                Console.WriteLine(serie);
+            }
+            else
+            {
+                Console.WriteLine("Serie inxesistante");
+            }
+        }
+
+        private static void AfficherLaListeDesGroupes(IDictionary<string, HashSet<Personnage>> groupes)
+        {
+            Console.WriteLine("\nListe des groupes : ");
+            foreach (string nomGroupe in groupes.Keys)
+            {
+                Console.WriteLine(nomGroupe);
+            }
+        }
+
+        private static void AfficherUnGroupe(string nom, Manager manager)
+        {
+            manager.Groupes.TryGetValue(nom, out HashSet<Personnage> personnages);
+            Console.WriteLine($"\nLe groupe \"{nom}\" est composé de :");
+            foreach (Personnage personnage in personnages)
+            {
+                Console.WriteLine(personnage);
+            }
+        }
+    }
+}
