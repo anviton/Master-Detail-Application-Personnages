e73a60d8c087089e1724947987b8de0804691eeb
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-20 07:52:18 +0000

    Début du binding sur MosaiquePersonnage_UC

--- Source/App/Vue_perso/UC_accueil/MosaiquePersonnages_UC.xaml.cs
+++ Source/App/Vue_perso/UC_accueil/MosaiquePersonnages_UC.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Modele;
 
 namespace Vue_perso
 {
@@ -18,9 +19,19 @@ namespace Vue_perso
     /// </summary>
     public partial class MosaiquePersonnages_UC : UserControl
     {
+        public Manager Mgr => (App.Current as App).MonManager;
+
         public MosaiquePersonnages_UC()
         {
             InitializeComponent();
+            DataContext = Mgr;
+        }
+
+        private void PersonnageSelectionne(object sender, SelectionChangedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this).Close();
         }
     }
 }
