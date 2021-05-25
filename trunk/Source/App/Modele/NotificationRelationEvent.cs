4bb1cbb672a1b0bbc05e9a0a1070a06f74effb8d
anviton <anviton@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-25 13:06:12 +0000

    Rajout de la classe NotificationRelationEvent.cs

---
+++ trunk/Source/App/Modele/NotificationRelationEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modele
+{
+    public class NotificationRelationEvent : EventArgs
+    {
+        public Relation LaRelation { get; private set; }
+        public Personnage LePersonnage { get; private set; }
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="personnage">Le personnage a notifié</param>
+        /// <param name="relation">La relation dans laquelle il est mentionné</param>
+        public NotificationRelationEvent(Personnage personnage, Relation relation)
+        {
+            LaRelation = relation;
+            LePersonnage = personnage;
+        }
+    }
+}
