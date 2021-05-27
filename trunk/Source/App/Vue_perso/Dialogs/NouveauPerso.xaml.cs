7af3d558fff8fefca168b8fdc69d35d1f46edb3e
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-05-27 07:49:01 +0000

    Ajout d'une boîte de dialogue fonctionnelle pour l'enregistrement d'un nouveau personnage (ATTENTION les ListBox ne se mettent pas à jour automatiquement)
    Renommage de l'ancien NouveauPerso en ModifierPerso
    
    MosaiqueSeries_UC est maintenant organisé en DockPanel et non en grille (l'ancienne disposition est en commentaire)

---
+++ trunk/Source/App/Vue_perso/Dialogs/NouveauPerso.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Modele;
+
+namespace Vue_perso.Dialogs
+{
+    /// <summary>
+    /// Logique d'interaction pour NouveauPerso.xaml
+    /// </summary>
+    public partial class NouveauPerso : Window
+    {
+        public Manager Mgr => (App.Current as App).MonManager;
+        public string NomPerso { get; set; }
+        public Serie SerieDuPerso { get; set; }
+
+        public NouveauPerso()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void NouvelleSerie(object sender, RoutedEventArgs e)
+        {
+            NouvelleSerieDialog dialog = new NouvelleSerieDialog();
+            
+            if (dialog.ShowDialog() == true)
+            {
+                if (Mgr.AjouterSerie(dialog.NomSerie, out Serie serie))
+                {
+                    MessageBox.Show($"La série \"{dialog.NomSerie}\" a été ajoutée.", "Nouvelle série", MessageBoxButton.OK, MessageBoxImage.Information);
+                    SeriesBox.SelectedItem = serie;
+                }
+                else
+                {
+                    MessageBox.Show($"La série \"{dialog.NomSerie}\" existe déjà !", "Nouvelle série", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void OKButtonPressed(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void CancelButtonPressed(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
---
+++ trunk/Source/App/Vue_perso/ModifierPerso.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vue_perso
+{
+    /// <summary>
+    /// Logique d'interaction pour ModifierPerso.xaml
+    /// </summary>
+    public partial class ModifierPerso : Window
+    {
+        public ModifierPerso()
+        {
+            InitializeComponent();
+        }
+
+        private void RenseignerChamp(object sender, RoutedEventArgs e)
+        {
+            Button senderButton = sender as Button;
+            Window newWindow;
+            /*
+             * On regarde quel bouton a été cliqué.
+             * Ensuite, on instancie un Window, contenant une référence vers une fenêtre demandée.
+             */
+            if (senderButton.Equals(ThemeButton))
+            {
+                newWindow = new ThemeMusical();
+                newWindow.ShowDialog();
+            } // Reste des fenêtres ici
+            if (senderButton.Equals(Parcourir))
+            {
+                newWindow = new ThemeMusical();
+                newWindow.ShowDialog();
+            }
+        }
+
+    }
+}
