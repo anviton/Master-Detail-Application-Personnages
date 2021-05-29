0161a6b2711474748766b53caf4fdf9dfd7e68c3
Antoine Viton <76434299+anviton@users.noreply.github.com>
2021-05-29 20:10:17 +0000

    Création d'une page Main_WindowGroupes.xaml en attendant de trouver une meilleure solution. Mise à jour et correction de la documentation. Modifications diag_de_seq_Supprimer_Un_personnage.png.

---
+++ Source/App/Vue_perso/Main_WindowGroupes.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Modele;
+using Vue_perso.Dialogs;
+
+namespace Vue_perso
+{
+    /// <summary>
+    /// Logique d'interaction pour Main_WindowGroupes.xaml
+    /// </summary>
+    public partial class Main_WindowGroupes : Window
+    {
+        public Manager Mgr => (App.Current as App).MonManager;
+        public Main_WindowGroupes()
+        {
+            InitializeComponent();
+            DataContext = Mgr;
+            Mgr.ListeDePersonnagesActive = Mgr.Groupes[Mgr.GroupeSelectionne];
+            HeaderListe.Text = $"Personnages de {Mgr.GroupeSelectionne}";
+        }
+
+        private void SupprimerPersonnageClick(object sender, RoutedEventArgs e)
+        {
+            if (Mgr.PersonnageSelectionne != null)
+            {
+
+                SuppressionConfirmation window = new SuppressionConfirmation();
+                window.ShowDialog();
+            }
+
+
+            Mgr.ListeDePersonnagesActive = Mgr.Personnages;
+
+
+        }
+
+        private void ModifierPersonnageClick(object sender, RoutedEventArgs e)
+        {
+            if (Mgr.PersonnageSelectionne != null)
+            {
+                var window = new ModifierPerso(Mgr.PersonnageSelectionne);
+                window.ShowDialog();
+            }
+        }
+        private void AjouterAUnGroupe(object sender, RoutedEventArgs e)
+        {
+            if (Mgr.PersonnageSelectionne != null)
+            {
+                var window = new AjouterAUnGroupe();
+                window.Show();
+            }
+        }
+    }
+}
