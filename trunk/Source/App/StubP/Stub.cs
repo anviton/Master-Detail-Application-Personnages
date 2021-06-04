7217a2cb85f8bd28790746766e4585c6b6409e6e
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-06-04 13:22:23 +0000

    Modification du fonctionnement de la classe ThemeMusical pour gérer l'ajout et la suppression des titres (que ce soit un leitmotiv ou non)

--- trunk/Source/App/StubP/Stub.cs
+++ trunk/Source/App/StubP/Stub.cs
@@ -38,16 +38,14 @@ namespace StubP
             mario.AjouterUnJeu("Super Mario Bros.", 1982, out j);
             mario.AjouterUnJeu("Super Mario Bros. 2", 1983, out j);
             mario.Description = "Personnage emblématique du monde du Jeu Vidéo";
-            bowser.Theme = new ThemeMusical(false); //je l'ai mis à true car sinon l'ajout ne marche pas (Du coup j'ai rien modifié dans la classe theme musical parce que j'ai pas trop compris ce que tu veux faire)
             bowser.Theme.AjouterTitre("Bowser's Theme", "https://www.youtube.com/watch?v=bq_jS6o3OoY");
             madeline.AjouterCitation("This is it, Madeline. Just breathe.");
-            madeline.Theme = new ThemeMusical(true);
+            madeline.Theme.Leitmotiv = true;
             madeline.Theme.AjouterTitre("First Steps");
             madeline.Theme.AjouterTitre("Reach for the Summit");
             groupes.Add("Triforce", new ObservableCollection<Personnage>());
             groupes["Triforce"].Add(link);
             return (LesSeries, groupes);
-
         }
         public void Sauvegarder(SeriesTheque lesSeries, IDictionary<string, ObservableCollection<Personnage>> groupes)
         {
