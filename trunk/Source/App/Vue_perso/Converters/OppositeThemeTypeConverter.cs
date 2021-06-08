3b90b0cea8e1b4bb85a33ad2011d10578ee5c0fe
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-06-08 15:28:21 +0000

    Thème musical fonctionnel (les tests sont à modifier et la mise en page de la boîte de dialogue est à corriger)

---
+++ trunk/Source/App/Vue_perso/Converters/OppositeThemeTypeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Vue_perso.Converters
+{
+	/// <summary>
+	/// Classe permettant d'afficher le texte adéquat
+	/// dans le bouton permettant de changer le type de thème musical d'un personnage
+	/// (Leitmotiv ou Musique unique)
+	/// </summary>
+	class OppositeThemeTypeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if ((bool)value == true) // S'il s'agit d'un Leitmotiv :
+			{
+				return "Passer au mode \"Musique unique\""; // Le bouton doit proposer de passer au mode "non-leitmotiv"
+			}
+			else // S'il ne s'agit pas d'un Leitmotiv :
+			{
+				return "Passer au mode \"Leitmotiv\""; // Le bouton doit proposer de passer au mode "leitmotiv"
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
