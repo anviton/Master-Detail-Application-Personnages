4a1080502c9161b0c38cf989b7c5d3c2c51621d6
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-06-12 21:52:07 +0000

    Données de l'application stockées dans AppData\Roaming

--- Source/App/AppPersonnageBin/setup/Readme.txt
+++
@@ -1,3 +0,0 @@
-Une fois l'application installée il est nécessaire de l'exécuter en tant qu'administrateur si vous souhaitez modifier des choses
-au sein de cetter dernière. Si ne vous n'exécutez pas l'appli en tant qu'administrateur vous pourrez seulement 
-consulter l'applicatoin.
--- Source/App/Vue_perso/App.xaml.cs
+++ Source/App/Vue_perso/App.xaml.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Modele;
-using StubP;
 
 namespace Vue_perso
 {
@@ -15,12 +14,10 @@ namespace Vue_perso
     /// </summary>
     public partial class App : Application
     {
-        //public Manager MonManager { get; private set; } = new Manager(new StubP.Stub());
         public Manager MonManager { get; private set; } = new Manager(new DataContractPersistance.DataContractPers());
 
         public App()
         {
-            //Chargeur chargeur = new Stub("");
             MonManager.ChargeDonnees();
         }
     }
