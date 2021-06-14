6dae2596d9ab6be0a724c1dab76c7f2b7b1601ba
thmaillarb <thmaillarb@48436599-dbb6-4d77-9694-e5f3a160831c>
2021-06-14 15:58:39 +0000

    Finalisation d'une première version du fichier de preuves et mise à jour de la documentation du constructeur de JeuVideo

--- trunk/Source/App/Modele/Relation.cs
+++ trunk/Source/App/Modele/Relation.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Constructeur : comment le faire ???
- * Soit on définit AvecPersoRec en fonction du constructeur appelé
- * (si il a pour argument persoRec ou NomPersoNonRec), soit c'est un argument du constructeur.
- * À mon avis, c'est mieux la première solution mais je sais pas si c'est exploitable
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
